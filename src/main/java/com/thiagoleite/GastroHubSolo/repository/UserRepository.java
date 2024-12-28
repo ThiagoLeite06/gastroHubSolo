5416acc5b65c2d77c20318eea302bdd0f6adeeae
Thiago Leite <thiago.0686@gmail.com>
2024-12-27 23:57:10 -0300

    create validation on model

--- src/main/java/com/thiagoleite/GastroHubSolo/repository/UserRepository.java
+++ src/main/java/com/thiagoleite/GastroHubSolo/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.thiagoleite.GastroHubSolo.repository;
 
 import com.thiagoleite.GastroHubSolo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
