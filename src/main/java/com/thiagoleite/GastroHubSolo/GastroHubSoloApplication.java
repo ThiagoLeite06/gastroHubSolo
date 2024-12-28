5c413a659840b3a2ffd558cab0d5be0a741892e8
Thiago Leite <thiago.0686@gmail.com>
2024-12-27 23:24:15 -0300

    Initial commit

---
+++ src/main/java/com/thiagoleite/GastroHubSolo/GastroHubSoloApplication.java
@@ -0,0 +1,13 @@
+package com.thiagoleite.GastroHubSolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastroHubSoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastroHubSoloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiagoleite/GastroHubSolo/GastroHubSoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagoleite.GastroHubSolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastroHubSoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
