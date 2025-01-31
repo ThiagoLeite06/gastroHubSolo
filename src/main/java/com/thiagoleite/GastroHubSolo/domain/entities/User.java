c934482df0d160a21544406fc29cdd3a0ac6bff1
Thiago Leite <thiago.0686@gmail.com>
2025-01-31 00:39:08 -0300

    feat: create user entity

---
+++ src/main/java/com/thiagoleite/GastroHubSolo/domain/entities/User.java
@@ -0,0 +1,29 @@
+package com.thiagoleite.GastroHubSolo.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import java.util.Date;
+
+@EqualsAndHashCode
+@Getter
+@Table(name = "users")
+public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private Date lastUpdatedAt;
+    private String address;
+
+    public User(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+}
+
